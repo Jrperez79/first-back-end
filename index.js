f5c3a34c4b42651a2b75b6951573a2eb304cc89c
Jrperez79 <joelrperez@gmail.com>
2020-08-11 17:42:56 -0700

    updated endpoints all working

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const request = require('superagent');
-const weatherData = require('./data/weather.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -10,7 +9,7 @@ app.use(cors());
 
 app.use(express.static('public'));
 
-const { GEOCODE_API_KEY } = process.env;
+const { GEOCODE_API_KEY, WEATHER_API_KEY, HIKING_API_KEY } = process.env;
 
 async function getLatLong(cityName) {
     const response = await request.get(`https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${cityName}&format=json`);
@@ -35,26 +34,59 @@ app.get('/location', async(req, res) => {
     }
 });
 
-function getWeather(lat, lon) {
-    // will make a call to the API call--for now it is hard coded
-    const data = weatherData.data;
+async function getWeather(lat, lon) {
+    const response = await request.get(`https://api.weatherbit.io/v2.0/forecast/daily?&lat=${lat}&lon=${lon}&key=${WEATHER_API_KEY}`);
 
-    const forecastArray = data.map((weatherItem) => {
+    const cityForecast = response.body.data;
+
+    const forecastArray = cityForecast.map((weatherItem) => {
         return {
             forecast: weatherItem.weather.description,
             time: new Date(weatherItem.ts * 1000)
         };
     });
-
-    return forecastArray;
+    return forecastArray.slice(0, 7);
 }
 
-app.get('/weather', (req, res) => {
+app.get('/weather', async(req, res) => {
     try {
         const userLat = req.query.latitude;
         const userLon = req.query.longitude;
 
-        const mungedData = getWeather(userLat, userLon);
+        const mungedData = await getWeather(userLat, userLon);
+        res.json(mungedData);
+    } catch (e) {
+        res .status(500).json({ error: e.message });
+    }
+});
+
+async function getHikingTrails(lat, lon) {
+    const response = await request.get(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=200&key=${HIKING_API_KEY}`);
+
+    const hikes = response.body.trails;
+
+    const trails = hikes.map(hike => {
+        return {
+            trail_url: hike.url,
+            name: hike.name,
+            location: hike.location,
+            condition_date: new Date(hike.conditionDate).toDateString(),
+            condition_time: new Date(hike.conditionDate).toTimeString(),
+            conditions: hike.conditionStatus,
+            stars: hike.stars,
+            star_votes: hike.starVotes,
+            summary: hike.summary
+        };
+    });
+    return trails;
+}
+
+app.get('/trails', async(req, res) => {
+    try {
+        const userLat = req.query.latitude;
+        const userLon = req.query.longitude;
+
+        const mungedData = await getHikingTrails(userLat, userLon);
         res.json(mungedData);
     } catch (e) {
         res .status(500).json({ error: e.message });
